a44df9118f5a75ecdd7accda210a23a253d4680e
Tomonori Tachi <67524847+tomosukesan@users.noreply.github.com>
2022-10-20 11:21:45 +0900

    finish ft_lstclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,31 +6,20 @@
 /*   By: ttachi <ttachi@student.42tokyo.ja>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 22:37:30 by ttachi            #+#    #+#             */
-/*   Updated: 2022/10/20 10:37:57 by ttachi           ###   ########.fr       */
+/*   Updated: 2022/10/20 11:21:03 by ttachi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
  #include "libft.h"
 
-void	del(void *arg)
+void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
-	free(arg);
-}
- void	ft_lstclear(t_list **lst, void (*del)(void*))
-{
-	t_list	*del_node;
-	del_node = (*lst)->next;
-	while (del_node != NULL)
+	t_list	*tmp;
+
+	while (*lst)
 	{
-		del((*lst)->content);
-		del_node = (*lst)->next->next;
-		free((*lst)->next);
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = tmp;
 	}
-
-}
-
-int main(void)
-{
-	t_list	*lst;
-	ft_lstclear(&lst, del);
 }
