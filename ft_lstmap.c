d1c320c8c99b4c6540f0a9d7f95bc9cc7a48541d
ttachi <ttachi@student.42tokyo.jp>
2022-10-22 22:55:47 +0900

    confirm ft_lst and ft_strnstr

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ttachi <ttachi@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 11:57:10 by ttachi            #+#    #+#             */
-/*   Updated: 2022/10/21 22:02:38 by ttachi           ###   ########.fr       */
+/*   Updated: 2022/10/22 22:35:37 by ttachi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,26 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
+	t_list	*node;
+	void	*new_content;
 
-	new_lst = malloc(sizeof(t_list));
-	if (new_lst == NULL)
-		return (NULL);
-	new_lst = lst;
-	ft_lstiter(new_lst, (void *)f);
-	if (new_lst == NULL)
-		ft_lstdelone(lst, (void *)del);
-	// (void)del;
+	// new_lst = malloc(sizeof(t_list));
+	// if (new_lst == NULL)
+	// 	return (NULL);
+	// printf("", sizeof(t_list));
+	new_lst = NULL;
+	while (lst != NULL)
+	{
+		new_content = f(lst->content);
+		node = ft_lstnew(new_content);
+		if (node == NULL)
+		{
+			ft_lstclear(&new_lst, del);
+			free(new_lst);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, node);
+		lst = lst->next;
+	}
 	return (new_lst);
 }
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ttachi <ttachi@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 20:15:19 by ttachi            #+#    #+#             */
-/*   Updated: 2022/10/21 20:28:24 by ttachi           ###   ########.fr       */
+/*   Updated: 2022/10/22 19:36:18 by ttachi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		all_split_c(char const *s, char c);
 // static
 char	*get_word(char const *s, char c, int i, char *result);
 // static
-char	*put_tail(char **head, char c, int str_num, int i);
+char	*put_tail(char **head, char c, int i);
 
 char	**ft_split(char const *s, char c)
 {
@@ -57,7 +57,7 @@ int	count_str(char const *s, char c)
 	char	*mov_p;
 
 	str_num = 0;
-	if ((s != NULL && s[0] == c) && s[0] != '\0')
+	if ((s != NULL && s[0] != '\0') && s[0] == c)
 		str_num--;
 	mov_p = (char *)s;
 	while (mov_p != NULL && *(mov_p) != '\0')
@@ -65,7 +65,7 @@ int	count_str(char const *s, char c)
 		mov_p = ft_strchr((const char *)mov_p, (int)c);
 		while ((mov_p != NULL && *(mov_p) != '\0') && *(mov_p + 1) == c)
 			mov_p++;
-		if (mov_p && *mov_p != '\0')
+		if (mov_p != NULL && *mov_p != '\0')
 			mov_p++;
 		str_num++;
 	}
@@ -76,36 +76,34 @@ int	all_split_c(char const *s, char c)
 {
 // static
 	size_t	i;
-	size_t	flag;
+	size_t	count;
 	size_t	s_len;
 
 	i = 0;
-	flag = 0;
+	count = 0;
 	s_len = ft_strlen(s);
 	while (i < s_len)
 	{
 		if (s[i] == c)
-			flag++;
+			count++;
 		i++;
 	}
-	return ((int)(s_len - flag));
+	return ((int)(s_len - count));
 }
 
 char	*get_word(char const *s, char c, int i, char *result)
 {
 // static
-	int		str_num;
 	char	*head;
 	char	*tail;
 
-	str_num = -1;
 	head = (char *)s;
 	if (head == NULL)
 		return (NULL);
 	tail = NULL;
 	while (*head == c)
 		head++;
-	tail = put_tail(&(head), c, str_num, i);
+	tail = put_tail(&(head), c, i);
 	if (tail == NULL)
 		tail = ft_strchr((const char *)head, '\0');
 	result = malloc(sizeof(char) * (tail - head + 1));
@@ -115,11 +113,13 @@ char	*get_word(char const *s, char c, int i, char *result)
 	return (result);
 }
 
-char	*put_tail(char **head, char c, int str_num, int i)
+char	*put_tail(char **head, char c, int i)
 {
 // static
+	int		str_num;
 	char	*tail;
 
+	str_num = -1;
 	while (*head && (str_num++) <= i)
 	{
 		tail = ft_strchr((const char *)*head, (int)c);
