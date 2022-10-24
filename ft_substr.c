ffa6f2e4e3cda80b5ddb3966bce367034370f426
Tomonori Tachi <67524847+tomosukesan@users.noreply.github.com>
2022-10-24 18:45:12 +0900

    final check

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttachi <ttachi@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: ttachi <ttachi@student.42tokyo.ja>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 20:05:23 by ttachi            #+#    #+#             */
-/*   Updated: 2022/10/21 21:25:27 by ttachi           ###   ########.fr       */
+/*   Updated: 2022/10/23 17:04:51 by ttachi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*t;
+	char	*result;
 	size_t	s_len;
 
 	if (s == NULL)
@@ -24,17 +24,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		s_len = ft_strlen(s);
 	if (start >= s_len || len == 0)
 	{
-		t = malloc(sizeof(char));
-		if (t == NULL)
+		result = malloc(sizeof(char));
+		if (result == NULL)
 			return (NULL);
-		t[0] = '\0';
-		return (t);
+		result[0] = '\0';
+		return (result);
 	}
 	if (s_len < len)
 		len = s_len;
-	t = malloc(sizeof(char) * (len + 1));
-	if (t == NULL || &(s[start]) == NULL)
+	result = malloc(sizeof(char) * (len + 1));
+	//if (result == NULL || &(s[start]) == NULL)
+	if (result == NULL)
 		return (NULL);
-	ft_strlcpy(t, &(s[start]), len + 1);
-	return (t);
+	ft_strlcpy(result, &(s[start]), len + 1);
+	return (result);
 }
